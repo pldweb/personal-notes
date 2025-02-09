3820be72936f729ec451652b4b008d05683a1fce
Muhammad Rivaldi Fanani <muhammadrivaldifnni01@gmail.com>
2025-02-09 16:07:28 +0700

    Update Login Page

--- frontend/notes-app/src/components/Input/PasswordInput.jsx
+++ frontend/notes-app/src/components/Input/PasswordInput.jsx
@@ -1,9 +1,35 @@
-import React from 'react'
+import React, { useState } from 'react'
+import { FaRegEye, FaRegEyeSlash } from 'react-icons/fa6'
 
-const PasswordInput = () => {
-  return (
-    <div>PasswordInput</div>
-  )
+const PasswordInput = ({value, onChange, placeholder, }) => {
+
+    const  [isShowPassword, setIsShowPassword] = useState(false)
+
+
+    const toggleShowPassword = () => {
+        setIsShowPassword(!isShowPassword)
+    }
+
+    return (
+        <div className='flex items-center bg-transparent px-5 rounded border-[1.5px] mb-3'>
+            <input 
+            type={isShowPassword ? "text" : "password"} 
+            value={value} 
+            onChange={onChange} 
+            placeholder={placeholder || 'Password'} 
+            className='w-full text-sm bg-transparent py-3 mr-3 rounded outline-none'/>
+
+            {isShowPassword ? <FaRegEye
+                size={32}
+                className='text-primary cursor-pointer'
+                onClick={() => toggleShowPassword()}
+            /> :  <FaRegEyeSlash
+                size={32}
+                className='text-slate-400 cursor-pointer'
+                onClick={() => toggleShowPassword()}
+            />}
+        </div>
+    )
 }
 
 export default PasswordInput
--- frontend/notes-app/src/pages/Login/Login.jsx
+++ frontend/notes-app/src/pages/Login/Login.jsx
@@ -1,24 +1,65 @@
-import React from 'react'
+import React, { useState } from 'react'
 import Navbar from '../../components/Navbar/Navbar'
 import { Link } from 'react-router-dom'
 import PasswordInput from '../../components/Input/PasswordInput'
+import { validateEmail } from '../../utils/helper.js'
 
 const Login = () => {
+
+    const [email, setEmail ] = useState("")
+    const [password, setPassword] = useState("")
+    const [error, setError] = useState(null)
+
+    const handleLogin = async (e) => {
+        e.preventDefault()
+
+        if(!validateEmail(email)){
+            setError("Tolong masukkan email yang valid")
+            return 
+        }
+
+        if(!password){
+            setError("Tolong masukkan password yang valid")
+            return
+        }
+
+        setError("")
+
+        // Login API
+    
+    }
+
   return (
     <>
         <Navbar />
         <div className='flex items-center mt-28 justify-center'>
             <div className='w-96 border rounded bg-white px-7 py-10'>
-                <form action="" onSubmit={() => {}}>
+                <form action="" onSubmit={handleLogin}>
 
-                    <h4 className="text-2xl mb-7">Login</h4>
-                    <input type="text" placeholder='email' className='input-box' />
-                    <PasswordInput />
+                    <h4 className="text-2xl mb-7 font-medium">Login</h4>
+
+                    <input 
+                    type="email" 
+                    placeholder='email' 
+                    className='input-box'
+                    value={email}
+                    onChange={(e) => setEmail(e.target.value)}
+                    />
+
+
+                    <PasswordInput 
+                    value={password}
+                    onChange={(e) => setPassword(e.target.value)}
+                    />
+
+                    {error && <p className="text-red-500 text-xs pb-1">{error}</p>}
+
+
                     <button type='submit' className='btn-primary'>Login</button>
                     <p className="text-sm text-center mt-4">
                         Belum daftar?{" "} <Link to="/signup" className="font-medium text-primary underline">Buat akun baru</Link>
                     </p>
-                    
+
                 </form>
             </div>
         </div>
